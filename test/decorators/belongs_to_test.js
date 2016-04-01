b70458076f25e69857bdc6d75868eee90ab77696
Benjamin Stepp <me@benstepp.com>
2016-03-31 22:15:51 -0500

    Kind of jank the build_association method together
    Will need to revisit after reflection

--- test/decorators/belongs_to_test.js
+++ test/decorators/belongs_to_test.js
@@ -1,4 +1,4 @@
-import { ActiveRecord, belongs_to, Meta } from '../../lib'
+import { ActiveRecord, belongs_to } from '../../lib'
 
 describe('belongs_to', () => {
   it('adds getters and setters on the klass with the name of the relation', () => {
@@ -10,5 +10,22 @@ describe('belongs_to', () => {
     const _User = ActiveRecord(User).default
     const user = new _User()
     expect(user.role).to.exist
+  })
+
+  describe('build_association method', () => {
+    it('can build the associated model', () => {
+      class Role extends ActiveRecord.Base {}
+      const _Role = ActiveRecord(Role).default
+
+      class User extends ActiveRecord.Base {
+        @belongs_to('role')
+        noop() {}
+      }
+      const _User = ActiveRecord(User).default
+
+      const user = new _User()
+      const role = user.build_role({})
+      expect(role).to.be.instanceof(_Role)
+    })
   })
 })
