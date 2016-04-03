008099026561cbd53afdddbfb49b07ef253a62e9
Benjamin Stepp <me@benstepp.com>
2016-04-02 23:30:29 -0500

    Begin adding a Migrator to load and run migrations
    Need to research the various cli tools for nodejs to see which one can
    run the migrations / generate new ones.

---
+++ test/fixtures/migrate/201604021021_first_migration.js
@@ -0,0 +1,15 @@
+import { ActiveRecord } from '../../../lib'
+
+class FirstMigration extends ActiveRecord.Migration {
+
+  up() {
+
+  }
+
+  down() {
+
+  }
+
+}
+
+export { FirstMigration }
---
+++ test/migrations/migrator_test.js
@@ -0,0 +1,66 @@
+import { Migrator } from '../../lib/migrations'
+
+describe('Migrator', () => {
+  beforeEach(() => {
+    Migrator.reset()
+  })
+
+  describe('migration_paths', () => {
+    it('returns an array of string', () => {
+      expect(Migrator.migration_paths).to.be.an('array')
+    })
+
+    it('contains the default of "db/migrate"', () => {
+      expect(Migrator.migration_paths).to.include('db/migrate')
+    })
+
+    it('can be added to', () => {
+      Migrator.migration_paths.push('server/db/migrate')
+
+      expect(Migrator.migration_paths).to.include('server/db/migrate')
+      expect(Migrator.migration_paths).to.include('db/migrate')
+    })
+  })
+
+  describe('load_migrations', () => {
+    it('loads all migrations in the migration paths', async () => {
+      Migrator.migration_paths.push('test/fixtures/migrate')
+      await Migrator.load_migrations()
+      expect(Migrator.migrations).to.be.an('array')
+    })
+
+    it('automatically adds versions to the migrations', async () => {
+      Migrator.migration_paths.push('test/fixtures/migrate')
+      await Migrator.load_migrations()
+      Migrator.migrations.forEach(migration => {
+        expect(migration.version).to.exist
+        expect(migration.version).to.be.a('number')
+      })
+    })
+  })
+
+  describe('last version', () => {
+    it('returns a number', async () => {
+      Migrator.migration_paths.push('test/fixtures/migrate')
+      await Migrator.load_migrations()
+      expect(Migrator.last_version).to.exist
+      expect(Migrator.last_version).to.be.a('number')
+    })
+
+    it('returns undefined when no migrations are present', async () => {
+      Migrator.migration_paths.push('test/fixtures/migrate')
+      await Migrator.load_migrations()
+      Migrator.migrations = []
+      expect(Migrator.last_version).to.not.exist
+    })
+  })
+
+  describe('last migration', () => {
+    it('returns the last migration', async () => {
+      Migrator.migration_paths.push('test/fixtures/migrate')
+      await Migrator.load_migrations()
+      const last_version = Migrator.last_version
+      expect(Migrator.last_migration.version).to.eq(last_version)
+    })
+  })
+})
