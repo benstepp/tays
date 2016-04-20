4a60d832bfe10bcbbdf8462eb8b26fcd21e12cee
Benjamin Stepp <me@benstepp.com>
2016-04-19 23:55:54 -0500

    So you can add and drop columns in alter table
    * Adds the AddColumn AlterColumn and DropColumn Unary nodes.
    * Adds methods to create these.
    * Adds a maybe visit for visiting column types

--- test/migrations/migration_test.js
+++ test/migrations/migration_test.js
@@ -1,16 +1,27 @@
 import ActiveRecord from '../../lib'
+import { includes } from 'lodash'
+
+class TestMigration extends ActiveRecord.Migration {
+  change() {
+    this.create_table('users', t => {
+      t.string('email')
+    })
+
+    this.add_column('users', 'encrypted_password', 'string')
+  }
+}
 
 describe('Migration', () => {
   it('saves off statements', () => {
-    class TestMigration extends ActiveRecord.Migration {
-      change() {
-        this.create_table('users', t => {
-          t.string('email')
-        })
-      }
-    }
-
     const migration = new TestMigration()
     expect(migration.statements.length > 0).to.eq(true)
+  })
+
+  it('has a create table statement', () => {
+    const migration = new TestMigration()
+    const first = migration.statements[0]
+    const second = migration.statements[1]
+    expect(includes(first, 'CREATE TABLE'))
+    expect(includes(second, 'ALTER TABLE'))
   })
 })
---
+++ test/sequel/features/alter_table_test.js
@@ -0,0 +1,49 @@
+import * as Sequel from '../../../lib/sequel'
+import { feature } from '../support'
+
+describe('ALTER TABLE', () => {
+  describe('simple table', () => {
+    const users = new Sequel.Relation('users')
+    const ast = users.alter()
+
+    feature({
+      ast: [ast],
+      postgresql: 'ALTER TABLE "users"'
+    })
+  })
+
+  describe('with a attribute', () => {
+    const users = new Sequel.Relation('users')
+    const email = users.column('email').varchar(100)
+    const ast = users.alter().add(email)
+
+    feature({
+      ast: [ast],
+      postgresql: 'ALTER TABLE "users" ADD COLUMN "email" varchar(100)'
+    })
+  })
+
+  describe('with multiple attributes', () => {
+    const users = new Sequel.Relation('users')
+    const email = users.column('email').varchar(100)
+    const created_at = users.column('created_at').datetime()
+    const ast = users.alter().add(email, created_at)
+
+    feature({
+      ast: [ast],
+      postgresql: 'ALTER TABLE "users" ADD COLUMN "email" varchar(100), ADD COLUMN "created_at" timestamps'
+    })
+  })
+
+  describe('adding and dropping', () => {
+    const users = new Sequel.Relation('users')
+    const email = users.column('email').varchar(100)
+    const dumpster = users.column('dumpster')
+    const ast = users.alter().add(email).drop(dumpster)
+
+    feature({
+      ast: [ast],
+      postgresql: 'ALTER TABLE "users" ADD COLUMN "email" varchar(100), DROP COLUMN "dumpster"'
+    })
+  })
+})
