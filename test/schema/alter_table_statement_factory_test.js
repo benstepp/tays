e617f165526c3f4ca6f8d9b021ae9b185705c9c6
Benjamin Stepp <me@benstepp.com>
2016-04-22 22:24:33 -0500

    Add rename_column schema statement for migrations

--- test/schema/alter_table_statement_factory_test.js
+++ test/schema/alter_table_statement_factory_test.js
@@ -27,4 +27,16 @@ describe('AlterTableStatementFactory', () => {
     expect(includes(statement, 'RENAME TO')).to.eq(true)
     expect(includes(statement, '"things"')).to.eq(true)
   })
+
+  it('can rename a column', () => {
+    const factory = new AlterTableStatementFactory('users', {}, t => {
+      t.rename_column('things', 'stuffs')
+    })
+    const statement = factory.statement
+    expect(includes(statement, 'ALTER TABLE')).to.eq(true)
+    expect(includes(statement, 'RENAME COLUMN')).to.eq(true)
+    expect(includes(statement, '"stuffs"')).to.eq(true)
+    expect(includes(statement, 'TO')).to.eq(true)
+    expect(includes(statement, '"things"')).to.eq(true)
+  })
 })
