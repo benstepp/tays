f286bbdc001d0eab1a94fa6bf15867d547295689
Benjamin Stepp <me@benstepp.com>
2016-04-26 23:53:49 -0500

    Begin callbacks and registering them on classes
    Some refactoring will need to be done between the types of transformers,
    there is now the pre user klass and soon the post user klass stage of a
    transformer. These also need to be setup such that a user could register
    their own to hook into the model creation process

---
+++ test/callbacks/adding_callbacks_test.js
@@ -0,0 +1,13 @@
+import { ActiveRecord } from '../../lib'
+import { before_validation } from '../../lib'
+
+describe('AddingCallbacks', () => {
+  it('adds the callbacks', () => {
+    class Stuff extends ActiveRecord.Base {
+      @before_validation
+      do_stuff() {}
+    }
+    const _Stuff = ActiveRecord(Stuff).default
+    expect(_Stuff.get_callbacks('before_validation').length).to.eq(1)
+  })
+})
